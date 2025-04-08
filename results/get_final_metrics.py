3322c04a1151e044d4e9d67e249e62afa4df68c7
Valdeira <pedromvaldeira@gmail.com>
2025-04-08 05:25:40 -0400

    minor change in default arg value

--- results/get_final_metrics.py
+++ results/get_final_metrics.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_clients', type=int, default=4)
     parser.add_argument('--metric', choices=['acc', 'f1'], required=True)
     parser.add_argument('--p_miss_train', nargs='+', default=['0.0', '0.1', '0.5']) # 0.0|0.1|0.5|None
-    parser.add_argument('--p_miss_test', nargs='+', default=['0.0', '0.1', '0.5', None]) # 0.0|0.1|0.5|None
+    parser.add_argument('--p_miss_test', nargs='+', default=['0.0', '0.1', '0.5']) # 0.0|0.1|0.5|None
     args = parser.parse_args()
 
     for p_miss_train in args.p_miss_train:
@@ -53,3 +53,5 @@ if __name__ == '__main__':
             metric_name = f"final_test_{args.metric}_{p_miss_test}"
             run_names = [f"{args.task_name}_{args.method}_K{args.num_clients}_p_miss_train{p_miss_train}_s{i}" for i in range(5)]
             main(args.project_name, run_names, metric_name)
+
+# python results/get_final_metrics.py --task_name hapt --method local --metric acc --p_miss_train 0.0 --p_miss_test 0.0 0.1 0.5
